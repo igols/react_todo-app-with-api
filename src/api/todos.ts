acaac71b9c724e3d961b792e442cd0b7244bd2f8
Igor <igor.olshanskyi@gmail.com>
2025-04-09 19:46:21 +0300

    base_code

---
+++ src/api/todos.ts
@@ -0,0 +1,16 @@
+import { Todo } from '../types/Todo';
+import { client } from '../utils/fetchClient';
+
+export const USER_ID = 2456;
+
+export const getTodos = () => {
+  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+};
+
+export const addTodos = (newTodo: Omit<Todo, 'id'>) => {
+  return client.post<Todo>(`/todos`, newTodo);
+};
+
+export const deleteTodos = (id: number) => {
+  return client.delete(`/todos/${id}`);
+};
